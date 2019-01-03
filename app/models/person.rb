90ddbb3851b550079bf9a0e11036146081ed7a24
Eric London <github@ericlondon.com>
2019-01-03 16:02:10 -0500

    person model validate email

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,5 @@ class Person < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
