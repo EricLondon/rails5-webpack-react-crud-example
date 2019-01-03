148202d2e8afc79a7d55095971adc4f725aaff5f
Eric London <github@ericlondon.com>
2019-01-03 15:38:56 -0500

    rake db:migrate

--- db/migrate/20190103202727_create_people.rb
+++ db/migrate/20190103202727_create_people.rb
@@ -1,9 +1,9 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
